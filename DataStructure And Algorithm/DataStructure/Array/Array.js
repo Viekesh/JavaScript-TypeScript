fd001dd1b803249ef3d8d62c860c5e655c283e52
Viekesh <vikeshgaikwad1@gmail.com>
2022-10-23 09:03:28 +0100

    Basic Array Operations

---
+++ DataStructure And Algorithm/DataStructure/Array/Array.js
@@ -0,0 +1,40 @@
+// Basic Array Operations :
+
+
+
+let data = [1, 4, 2, 5, 2, 4, 3, 9, 7, 8];
+
+// for (i = 0; i < data.length; i++) {
+// document.write(data[i]);
+// document.write(`${data[i]} <br />`);
+
+
+
+// document.write(`Array ${i} is ${data[i]} <br />`)
+// let result = (`Array ${i} is ${data[i]} <br />`);
+// document.getElementById("arr").innerHTML = result;
+// }
+
+// let x = 3;
+// document.write(data[x]);
+
+
+
+// const getElement = () => {
+//     let el = document.getElementById("arr").value;
+//     // alert("You entered " + el);
+//     alert(data[el]);
+// };
+
+
+
+// const getElement = () => {
+//     let el = document.getElementById("arr").value;
+
+//     if(el < data.length) {
+//         alert("You enter " + el);
+//         alert(data[el] + " is on the position of " + el + "th");
+//     } else {
+//         alert("Enter valid number");
+//     }
+// }
