19d6b8425377911f3f2a40c07bb8ccda476b9ca1
Viekesh <vikeshgaikwad1@gmail.com>
2022-12-10 10:55:13 +0000

    Initialise React App

---
+++ ReactJs/shopping/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Demo from './Projects/Demo';
+
+function App() {
+  return (
+    <>
+      <Demo />
+    </>
+  );
+}
+
+export default App;
---
+++ ReactJs/shopping/src/Projects/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hello = () => {
+
+    const title = "amazon shopping"
+  return (
+    <>{title}</>
+  )
+}
+
+export default Hello;
