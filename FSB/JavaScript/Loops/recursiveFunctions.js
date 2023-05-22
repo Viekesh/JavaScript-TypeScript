6a9575bd8e23a7c42ba2e7fa35edef4093832b44
Viekesh <vikeshgaikwad1@gmail.com>
2023-05-22 10:59:19 +0530

    while and do...while loop completed

---
+++ FSB/JavaScript/Loops/recursiveFunctions.js
@@ -0,0 +1,4 @@
+
+
+
+// Here we build our own loop by way of functions that can recursively call themselves. We saw some loop constructs and modules. One of the things that we can do in our function is to invoke itself
--- FSB/JavaScript/Loops/while...and...doWhile.js
+++ FSB/JavaScript/Loops/while...and...doWhile.js
@@ -102,7 +102,6 @@ console.log(result);
 
 // Syntax :
 
-
 // do
   // statement
 // while (condition);
@@ -128,3 +127,121 @@ do {
 // Despite i === 0 this will still loop as it starts off without the test
 
 console.log(output);
+
+// important difference in do...while loop is that the do...while loop execute your code in block atleast one times because here the condition is validated after the code has been executed atleast once. Whereas in the case of the regular while loop the condition is validated first before the code block can be executed.
+
+// lets look at the while loop, here we'll use an array of odd numbers.
+
+// we created counter variable m and an array oddNums.
+
+// let m = 1;
+// const oddNums = [];
+
+// Now we can express the while loop where we'll set our counter to be less than 40 because we want odd number from 1 to 40 and inside we use "push" to insert the current value of our counter variable "m" into the array before incrementing the counter by two.
+
+// while (m < 40) {
+//   oddNums.push(m);
+//   m += 2;
+// }
+
+// console.log(oddNums.toString(), ": while");
+
+// So, the first time this runs "m" will be one. It will be inserted into the array then the addition assignment operator will set "m" to 3 and the loop runs again. The condition is again satisfied and it keeps running like that. When the condition turns falsy, the loop ceases to run and terminates.
+
+// Also notice the using of "toString()" method to output the contents of the array as a comma separated string.
+
+while (m < 1) {
+  oddNums.push(m);
+  m += 2;
+}
+
+console.log(oddNums.toString(), ": (m < 1), while...loop");
+
+// If we set "m" to less than 1, in which case the condition is never truthy, you'll notice our code block never run at all and our array stays empty.
+
+
+// Now for the do...while loop we replicate the exact same setup. Notice how the code block appear after the "do" keyword followed by the condition. The overall outcome the while and do...while appears to be the same in this example
+
+// let n = 1;
+// const oddNumsAgain = [];
+
+// do{
+//   oddNumsAgain.push(n);
+//   n += 2;
+// } while (n < 40);
+
+// console.log(oddNumsAgain.toString(), ": do...while");
+
+// But notice what happens if we set the condition is "m" less than one, which will never evaluate as truthy. In the case of our "while" loop, the code block never runs, but in the case of do...while the code block was executed once before the codition check, which failed, hence the loop never iterated. This is why you do get 1.
+
+// let n = 1;
+// const oddNumsAgain = [];
+
+// do{
+//   oddNumsAgain.push(n);
+//   n += 2;
+// } while (n < 1);
+
+// console.log(oddNumsAgain.toString(), ": (n < 1) do...while");
+
+// The initial value of counter variable "n".
+
+// And just like all the loop we can use "break" keyword to focibly exit the loop.
+
+let n = 1;
+const oddNumsAgain = [];
+
+do{
+  oddNumsAgain.push(n);
+  if(n === 15) {
+    break;
+  }
+  n += 2;
+} while (n < 40);
+
+console.log(oddNumsAgain.toString(), ": (n < 40) do...while");
+
+// So, for instance if we want to stop when n hits 15, we use "if" block and breaks the loop once it hits 15.
+
+// ex : print even numbers using "while" loop and odd numbers using "do...while" loop
+
+let a = 0, b = 1;
+
+const evenNumbers = [], oddNumbers = [];
+
+while(a <= 100) {
+  evenNumbers.push(a);
+  a += 2;
+}
+
+console.warn(`Even Numbers from 0 to 100 : ${evenNumbers.toString()}`);
+
+do {
+  oddNumbers.push(b);
+  b += 2;
+} while( b <= 100);
+
+console.warn(`Odd Numbers from 0 to 100 : ${oddNumbers.toString()}`);
+
+
+
+// "while" loop :
+
+// The syntax of while loop looks as follows:
+// let n = 0
+// while(condition){
+// statement
+// }
+
+// As demonstrated, condition is Evaluated before executing the statement
+
+
+// "do...while" loop :
+// The syntax of do..while loop looks as follows:
+// let n = 0
+// do{
+// statement
+// } while(condition)
+// As demonstrated, condition is Evaluated after executing the statement.
+
+
