d5518a6bc0cdc93a1fb6f19987bc31b6c3c33867
Viekesh <vikeshgaikwad1@gmail.com>
2023-07-11 20:58:33 +0530

    Continue To Work With Shalow Copy And Deep Copy

--- FSB/JavaScript/Objects/ShallowAndDeepCopy.js
+++ FSB/JavaScript/Objects/ShallowAndDeepCopy.js
@@ -2,6 +2,226 @@
 
 
 
+// In a reassignment operation involving primitive data types such as strings, numbers, and booleans, the original variable is copied by JavaScript.
+
+// For example, consider the following code:
+
+// ex 1 :
+
+let x = 3
+let y = x // x is copied into y
+y++ // y is incremented
+console.log(y) // now 4
+console.log(x) // still 3
+
+// In this case, the value 3 is copied into y, and then x is disconnected from y. So mutating y does not affect x.
+
+// Conversely, with non-primitive data types like arrays and objects, only a reference to the values is passed. So when the copy is mutated, the original also gets mutated. This is also known as shallow copying.
+
+// ex 2 :
+
+let adam = { name: "Adam" };
+let jason = adam;
+jason.name = "Jason";
+console.log(adam.name);  // outputs "Jason"
+console.log(jason.name); // outputs "Jason"
+
+// If we instead want to copy an object so that we can modify it without affecting the original object, we need to make a deep copy.
+
+console.log("...");
+console.log("...");
+console.log("...");
+console.log("...");
+console.log("...");
+console.log("...");
+
+
+
+// 5 Ways to deep copy an object :
+
+// In JavaScript, we can perform a copy on objects using the following methods:
+
+// shallow copy with equal to ( = ) sign. It has clear and direct or you can say its a default feature of javascript but this method only create a shallow copy means it cannot copy a nested objects.
+
+// json.stringify and json.parse by using this we can able to create deep copies and nested copies but it doesn't copy a functions
+
+// Object.assign() it can copies the immediate members of an object such as functions but it doesn't deep copy nested objects.
+
+// the ...spread operator is a simple syntax a preffered way to copy a object but it doesn't copy a nested objects (it cannot create a deep copy).
+
+// lodash clonedeep() it can clones nested objects including functions and adds an external dependancy to your project.
+
+
+
+// These methods all have their pros and cons. Let's take a closer look at each of them.
+
+// Shallow Copy an Object by Assignment
+
+// You can create a shallow copy of an object by simply assigning the original object to a new variable. 
+// Consider the following object:
+
+// ex 3:
+
+const user = {
+    name: "Kingsley",
+    age: 28,
+    job: "Web Developer"
+}
+
+// To create a copy of the object user, we assign the object to a new variable like so:
+
+const clone1 = user;
+
+console.log(" ex 2 : user : ", user);
+
+console.log(" ex 2 : clone1 : ", clone1);
+
+// output :
+
+// ex 2 : user :  { name: 'Kingsley', age: 28, job: 'Web Developer' }
+
+// ex 2 : clone :  { name: 'Kingsley', age: 28, job: 'Web Developer' }
+
+// As observed in the console output, we have now copied the object from user into clone1.
+
+// However, all we did was create a reference to the original object. Whenever we mutate a property in the object clone, we'll also end up mutating the original object (user) as we do in the following code:
+
+clone1.age = 30;
+
+console.log("clone1 : ", clone1);
+
+console.log("user : ", user);
+
+console.log("...");
+console.log("...");
+console.log("...");
+console.log("...");
+console.log("...");
+console.log("...");
+
+// So when a non-primitive data type (array or object) is assigned to a new variable, JavaScript makes a shallow copy of the original object.
+
+
+
+// Copy an Object With JSON.stringify() and JSON.parse()
+
+// The JSON.stringify() method takes in an object and creates a JSON string from it. The JSON.parse() method parses a string and returns a JavaScript object.
+
+// We can combine both of these methods to create a copy of an object in the following way:
+
+// ex 4:
+
+const user1 = {
+    name: "Kingsley",
+    age: 28,
+    job: "Web Developer"
+};
+
+console.log("ex4 : user1 :", user1);
+
+let clone3 = JSON.stringify(user1);
+
+console.log("ex4 : clone3 : ", clone3);
+
+let clone2 = JSON.parse(JSON.stringify(user1));
+
+console.log(user1);
+
+console.log(clone2);
+
+// When the copy object is mutated, the original object stays the same:
+
+clone2.age = 32;
+console.log(user1);
+console.log(clone2);
+
+console.log("...");
+console.log("...");
+console.log("...");
+console.log("...");
+console.log("...");
+console.log("...");
+
+
+// However, there is one caveat to using this approach: JSON.stringify() does not copy functions.
+
+// Suppose we have a method in our object user called incrementAge:
+
+const user33 = {
+    name: "Kingsley",
+    age: 28,
+    job: "Web Developer",
+    incrementAge: function() {
+  	  this.age++
+    }
+}
+
+// The function will not be available in the copied object. Thus, this method achieves deep copy only if there is no function within the object.
+
+
+
+// Copy an Object With Object.assign()
+// Before ES6, Object.assign() was the most popular way to deep copy an object.
+
+const user22 = {
+    name: "Kingsley",
+    age: 28,
+    job: "Web Developer",
+    incrementAge: function() {
+  	  this.age++
+    }
+}
+let clone22 = Object.assign({}, user) // Copies user into clone
+
+// Object.assign() will copy everything into the new object, including any functions. Mutating the copied object also doesn't affect the original object.
+
+clone22.age = 32;
+console.log(user22);
+console.log(clone22);
+/* 
+{ 
+age: 28, 
+incrementAge: function() { 
+this.age++ 
+}, 
+job: "Web Developer", 
+name: "Kingsley" 
+} 
+{ 
+age: 32, 
+incrementAge: function() { 
+this.age++ 
+}, 
+job: "Web Developer", 
+name: "Kingsley" 
+} 
+*/
+
+// However, one thing to remember about Object.assign() is that the method only performs a partial deep copy on objects.
+// To understand what that means, let's consider the following:
+
+const user = {
+    name: "Kingsley",
+    age: 28,
+    job: "Web Developer",
+    location: {
+      city: "Lagos",
+    }
+}
+const clone = Object.assign({}, user)
+
+console.log("...");
+console.log("...");
+console.log("...");
+console.log("...");
+console.log("...");
+console.log("...");
+
+
+
+
+
+
 // Copying an object seems like a simple task until you realize the way objects are stored in variables. We’ve studied this before. Objects are stored as references and not as literal values, so they cannot be reassigned in the same way as regular strings, numbers, or Boolean.
 
 // In fact, when you have to make a copy of an object, you have two choices.
@@ -68,7 +288,7 @@ const User = {
 
 
 
-// WRONG - we cannot clone this object using direct reassignment because variable //joeM points to the same object as the constant ‘User’.
+// WRONG - we cannot clone this object using direct reassignment because variable joeM points to the same object as the constant ‘User’.
 //const joeM = User;
 
 // The spread operator allows us to create a true but shallow clone of the object.
@@ -198,7 +418,7 @@ console.log(
 
 // This function iterates through the object and uses the spread operator to create clones if the property holds an object or an array. We're ultimately using the spread operator here as we saw earlier in an example, however, this function produces a deep clone by traversing through the object and all possible nested objects/arrays to produce a true clone.
 
-// Finally, modify the assignment to productB as shown below: 
+// Finally, modify the assignment to productB as shown below:
 
 // let productB = deepCopy(productA);
 
