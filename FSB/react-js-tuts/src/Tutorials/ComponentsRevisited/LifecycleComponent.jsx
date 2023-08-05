9c6baf316167ea9496b86ba90d7f1941b36f7ab4
Viekesh <vikeshgaikwad1@gmail.com>
2023-08-05 18:15:32 +0530

    Practice With React Lifecycle Components

--- FSB/react-js-tuts/src/Tutorials/ComponentsRevisited/LifecycleComponent.jsx
+++ FSB/react-js-tuts/src/Tutorials/ComponentsRevisited/LifecycleComponent.jsx
@@ -51,6 +51,16 @@
 
 // Lifecycle of components :
 
+// Lifecycle methods are called at different points in the life of a component. These methods can be used to perform tasks such as setting up the component, updating the state, or cleaning up when the component is no longer needed.
+
+// Here are some of the most commonly used lifecycle methods:
+
+// componentDidMount(): This method is called after the component has been mounted to the DOM.
+// componentDidUpdate(): This method is called after the component has been updated.
+// componentWillUnmount(): This method is called before the component is unmounted from the DOM.
+
+
+
 // There are three main phases in the life of a React component:
 
 // Mounting Phase :
@@ -84,3 +94,97 @@
 // The constructor is called before the component is mounted into the DOM. The typical use of a constructor is to initialize local state variables and bind class methods. This is the only lifecycle method where one can initialize state using this.state, as all other lifecycle class methods use this.setState().
 
 // Constructors should only be used for initializing state and binding methods since they’re called in the render phase where nothing has gone into the DOM.
+
+// ex :
+
+import React from "react";
+
+class MyConstructor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <button onClick={this.incrementCounter}>Increment</button>
+                <p>The counter is: {this.state.counter}</p>
+            </div>
+        )
+    }
+
+    incrementCounter = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
+}
+
+export default MyConstructor;
+
+
+
+// The constructor method is called when a new instance of the component is created. In this example, the constructor method is used to initialize the state of the component. The state is an object that can be used to store data that can change over time. In this example, the state is used to store the counter value.
+
+// The constructor method also has access to the props of the component. The props are the data that is passed to the component from its parent component. In this example, the props are not used in the constructor method.
+
+// Here are some other examples of things that you can do in the constructor method of a React component:
+
+// Bind event handlers to event listeners.
+// Initialize state variables.
+// Make API calls.
+// Load data from a file.
+// Initialize third-party libraries.
+// The constructor method is a powerful tool that can be used to set up your components and prepare them for rendering.
+
+
+
+// getDerivedStateFromProps() Method :
+
+// The getDerivedStateFromProps() method is a rare lifecycle method which is used only in very special cases.  
+// This lifecycle hook is used in both the mounting and updating phases and allows us to perform state changes on the basis of data in the props.
+
+// Remember,  getDerivedStateFromProps() method is invoked every time a parent component re-renders.
+
+
+
+// render() method :
+
+// The render method specifically is needed only if we’re building class components and its purpose is to return React elements. This is also the reason why the render method should stay pure, that is, must never modify the state and should always return the same, predictable result.
+
+// Mounting => Constructor => Render => Element inserted into the DOM
+
+
+
+// componentDidMount() method
+
+// Once the DOM insertion is complete, the componentDidMount() lifecycle method is immediately invoked. This is where we can perform API queries and other side effects which leads to setting the state.
+
+// Once the state is set, the component re-renders.
+
+
+
+
+
+
+// Updating Phase
+
+// The ‘Updating’ phase occurs when we interact with the component and it re-renders. This interaction includes receiving updated data through props or an update to the state. React has five built-in methods that is called in the order given below when a component is updated:
+
+
+
+// getDerivedStateFromProps() Method
+
+// Upon implementation, the getDerivedStateFromProps() method allows us to decide whether we want the state to be updated or not based on the changes made to the data in the props. In simple words, when getDerivedStateFromProps() method is called in the ‘Mounting’ phase, it can be used to set state variables based on the data in the props.
+
+// The next time the getDerivedStateFromProps() method is called in the ‘Updating’ phase, we can compare the data in the state with that in the props and we can update the state if needed.
+// This helps in the implementation of the state changes that will occur only if the data in the props differs from what it was earlier.
+
+// This is important to understand because every update to the parent component brings about an update to the child components, even if the data in the props continue to be the same.
+// If the data in the props continue to be the same as before, we may not want to update the state in a child component.
+
+// Mounting Phase => getDerivedStateFromProps() => state variables are set
